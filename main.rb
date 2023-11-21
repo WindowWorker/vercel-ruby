2e8fe621fea58456fe5cf1f5c1940e6d3d1d06de
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-21 17:36:00 +0000

    Ruby Test Server

--- main.rb
+++ main.rb
@@ -1,1 +1,11 @@
-puts 'Hello, World!'
+require_relative 'api/index.rb'
+require 'webrick'
+
+server = WEBrick::HTTPServer.new :Port => 8000
+trap 'INT' do server.shutdown end
+
+server.mount_proc '/' do |req, res|
+  Handler[req,res]
+end
+
+server.start
