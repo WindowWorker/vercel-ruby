c62982443944d49118f3b70a2263bf9941e48e33
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-22 05:39:00 +0000

    d

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative 'api/index.rb'
 require 'webrick'
 
 server = WEBrick::HTTPServer.new :Port => 8000
-trap 'INT' do server.shutdown end
+#trap 'INT' do server.shutdown end
 
 server.mount_proc '/' do |req, res|
   Handler[req,res]
