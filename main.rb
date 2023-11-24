6df8226acf936518e41f075b3dc0e1e38bbee358
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-11-24 18:22:02 +0000

    awdf

--- main.rb
+++ main.rb
@@ -1,11 +1,13 @@
-require_relative 'api/index.rb'
+require_relative 'api/index'
 require 'webrick'
 
-server = WEBrick::HTTPServer.new :Port => 8000
-#trap 'INT' do server.shutdown end
+if ENV['PATH'].include?('runner')
+  server = WEBrick::HTTPServer.new Port: 8000
+  # trap 'INT' do server.shutdown end
 
-server.mount_proc '/' do |req, res|
-  Handler[req,res]
+  server.mount_proc '/' do |req, res|
+    Handler[req, res]
+  end
+
+  server.start
 end
-
-server.start
