c74de6a78abd994f12821e01380657e770f86fe7
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-02 21:24:36 +0000

    asdf

--- api/link-resolver-js.rb
+++ api/link-resolver-js.rb
@@ -4,7 +4,7 @@ def link_resolver()
   
   return <<-TEXT
   if(!globalThis.hostTargetList){
-  globalThis.hostTargetList = ['www.ruby-lang.org','docs.ruby-lang.org','ruby-doc.org','ruby-doc.com','www.ruby-doc.org','www.ruby-doc.com'];
+  globalThis.hostTargetList = ['www.ruby-lang.org','docs.ruby-lang.org','try.ruby-lang.org','ruby-doc.org','ruby-doc.com','www.ruby-doc.org','www.ruby-doc.com'];
 
 }
 
