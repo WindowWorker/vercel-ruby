fda6105d33ae84a0cd26435081d1c45bb26a6d77
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-03 00:33:36 +0000

    asdf

--- api/rubystyle-css.rb
+++ api/rubystyle-css.rb
@@ -1,10 +1,22 @@
 def rubystyle()
   
  return <<-TEXT
-  #header_content>a{
-
-background:  url(https://archives.bulbagarden.net/media/upload/e/ed/Spr_5b_383.png) no-repeat left 8px !important;
+ 
+#header_content>a{
+  background:  url(https://archives.bulbagarden.net/media/upload/e/ed/Spr_5b_383.png) 
+  no-repeat left 8px !important;
   background-size: 20% !important;
 }
+
+html[window-location*="sapphire"] #header_content>a{
+  background:  url(https://archives.bulbagarden.net/media/upload/1/10/Spr_5b_382.png) 
+  no-repeat left 8px !important;
+  background-size: 20% !important;
+}
+
+html[window-location*="docs.ruby-lang.org"] a{
+color: #CC342D !important;
+}
+
 TEXT
 end
