baa0b89821e20d8882bc7d380f4ddb413de4fffb
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-03 00:50:01 +0000

    zip

--- api/rubystyle-css.rb
+++ api/rubystyle-css.rb
@@ -8,11 +8,19 @@ def rubystyle()
   background-size: 20% !important;
 }
 
+
+html[window-location*="sapphire"]{
+filter:hue-rotate(-120deg);
+}
+
 html[window-location*="sapphire"] #header_content>a{
   background:  url(https://archives.bulbagarden.net/media/upload/1/10/Spr_5b_382.png) 
   no-repeat left 8px !important;
   background-size: 20% !important;
+  filter:hue-rotate(120deg);
 }
+
+
 
 html[window-location*="docs.ruby-lang.org"] a{
 color: #CC342D !important;
