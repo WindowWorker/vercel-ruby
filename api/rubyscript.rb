b8cdd27ef58b72fb8766fca9f47efbea9cbb5f83
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-03 00:46:05 +0000

    asfd

--- api/rubyscript.rb
+++ api/rubyscript.rb
@@ -3,10 +3,14 @@ def rubyscript()
 
 return <<-TEXT
 
+void function rubyscript(){
+
 let ruby_html = document.querySelector('html');
 
 ruby_html.setAttribute('window-location' , window.location.href);
 ruby_html.setAttribute('user-agent' , navigator.userAgent);
+}();
+
 
 TEXT
 
