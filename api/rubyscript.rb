f41baf37944354089873814f6aea141357d58a89
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-03 00:59:31 +0000

    asdf

--- api/rubyscript.rb
+++ api/rubyscript.rb
@@ -9,7 +9,41 @@ let ruby_html = document.querySelector('html');
 
 ruby_html.setAttribute('window-location' , window.location.href);
 ruby_html.setAttribute('user-agent' , navigator.userAgent);
+
+function swapSapphireText(el){
+
+  if(!el){return;}
+  var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+  while(n=walk.nextNode()){
+  a.push(n);
+    let ntext=n.textContent;
+
+  ntext=ntext.replace(/ruby/gi,'Sapphire');
+
+  if(ntext!=n.textContent){
+    n.textContent=ntext;
+  }
+
+  };
+  if(document.title.toLowerCase().includes('ruby')){
+    document.title=document.title
+      .replace(/ruby/gi,'Sapphire')
+     }
+  return a;
+  }
+
+
+if(window.location.href.toLowerCase().includes('sapphire')){
+setInterval(function(){
+
+swapSapphireText(document.body);
+
+},100);
+
+}
+
 }();
+
 
 
 TEXT
