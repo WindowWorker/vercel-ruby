7e9b89cf6b543e5ded7e660281642bbadb10ab55
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-04 17:54:25 +0000

    asdf

--- api/rubystyle-css.rb
+++ api/rubystyle-css.rb
@@ -82,7 +82,7 @@ filter:hue-rotate(135deg);
 html[window-location*="emerald"] #header_content>a{
   background:  url(https://archives.bulbagarden.net/media/upload/5/55/Spr_5b_384.png) 
   no-repeat left 8px !important;
-  background-size: 25% !important;
+  background-size: 18% !important;
   background-position-y: bottom !important;
   filter:hue-rotate(-135deg);
 }
