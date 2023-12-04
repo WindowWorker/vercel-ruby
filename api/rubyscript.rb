e3b16f8f38cad51b85f7baa01bb9f207ef4045b3
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-04 14:27:41 +0000

    asdf

--- api/rubyscript.rb
+++ api/rubyscript.rb
@@ -7,8 +7,8 @@ void function rubyscript(){
 
 let ruby_html = document.querySelector('html');
 
-ruby_html.setAttribute('location-href' , window.location.href);
-ruby_html.setAttribute('navigator-user-agent' , navigator.userAgent);
+ruby_html.setAttribute('window-location' , window.location.href);
+ruby_html.setAttribute('user-agent' , navigator.userAgent);
 if (window!=window.top){
 ruby_html.setAttribute('framed','');
 }
