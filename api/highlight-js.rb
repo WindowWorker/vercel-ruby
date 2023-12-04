63ac94d57c5d13941028dd6e2bb6dba5a6ec3724
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-04 16:28:42 +0000

    asdf

--- api/highlight-js.rb
+++ api/highlight-js.rb
@@ -7,6 +7,21 @@ def highlight()
  return <<-TEXT
 
 
+globalThis.highlighter=async function(){
+  if(document.querySelector('[class*="language-"]')){
+  try{
+    if(globalThis.Prasm){
+      let hs=Array.from(document.querySelectorAll('[class*="language-"]'));
+      let hs_length=Math.min(hs.length,77);
+      for(let i=0 ; i<hs_length ; i++){try{
+        Prism.highlightElement?.(hs[i]);
+      }catch(e){await(console.log(e));continue;}}
+      }
+    }catch(e){await(console.log(e));}
+  }
+}
+
+
 declare(async ()=>{
 if(!(document.querySelector('link[href="https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css"]'))){
   let prismCss = document.createElement('link');
@@ -24,7 +39,7 @@ for(let i=0;i<prs_length;i++){try{
   prs[i].setAttribute('class','language-ruby');
 }catch(e){await console.log(e); continue;}}
 if(document.querySelector('[class*="language-"]')){
-Prism.highlightAll();
+highlighter();
 }
 
 });
