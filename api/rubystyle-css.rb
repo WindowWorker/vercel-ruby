733c743c708e56394cd163979b3b312fa4e749e3
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-04 15:29:46 +0000

    asdf

--- api/rubystyle-css.rb
+++ api/rubystyle-css.rb
@@ -17,27 +17,42 @@ def rubystyle()
     background-size: 25% !important;
     background-position-y: bottom !important;
   }
-}
-html[window-location*="sapphire"]:not([framed]){
-filter:hue-rotate(-120deg);
-}
-
-
-@media screen and (min-width: 768px) {
-
-  html[window-location*="sapphire"] #header_content>a{
-    background:  url(https://archives.bulbagarden.net/media/upload/1/10/Spr_5b_382.png) 
-    no-repeat left 8px !important;
-    background-size: 25% !important;
-    background-position-y: bottom !important;
-    filter:hue-rotate(120deg);
-  }
-
+  
 }
 
 html[window-location*="ruby-doc.com"] #header{
 background:url(https://archives.bulbagarden.net/media/upload/7/7d/Spr_3e_383.png) 850px 0 no-repeat !important;
 }
+
+
+
+html[window-location*="sapphire"]:not([framed]){
+filter:hue-rotate(-120deg);
+}
+
+html[window-location*="sapphire"] #header_content>a{
+  background:  url(https://archives.bulbagarden.net/media/upload/1/10/Spr_5b_382.png) 
+  no-repeat left 8px !important;
+  background-size: 25% !important;
+  background-position-y: bottom !important;
+  filter:hue-rotate(120deg);
+}
+
+
+@media screen and (min-width: 768px) {
+
+ html[window-location*="sapphire"] a.download-link{
+    background:#CC342D url(https://archives.bulbagarden.net/media/upload/c/c6/Ani382OD.png) no-repeat 8px center !important;
+    background-size: 25% !important;
+    background-position-y: bottom !important;
+  }
+
+}
+
+html[window-location*="sapphire"][window-location*="ruby-doc.com"] #header{
+background:url(https://archives.bulbagarden.net/media/upload/8/80/Spr_3e_382.png) 850px 0 no-repeat !important;
+}
+
 
 html[window-location*="docs.ruby-lang.org"] a,
 html[window-location*="ruby-doc.com"] a,
