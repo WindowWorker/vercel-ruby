43631f331387fdc77cb5129fb7aeb354031f7bf0
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-04 14:26:35 +0000

    asdf

--- api/rubyscript.rb
+++ api/rubyscript.rb
@@ -7,13 +7,22 @@ void function rubyscript(){
 
 let ruby_html = document.querySelector('html');
 
-ruby_html.setAttribute('window-location' , window.location.href);
-ruby_html.setAttribute('user-agent' , navigator.userAgent);
+ruby_html.setAttribute('location-href' , window.location.href);
+ruby_html.setAttribute('navigator-user-agent' , navigator.userAgent);
 if (window!=window.top){
 ruby_html.setAttribute('framed','');
 }
 
-
+Element.prototype.updateAttribute=function(attr,val){
+const el = this;
+if(!(el.hasAttribute(attr,val))){
+el.setAttribute(attr,val);
+}else{
+if(el.getAttribute(attr)!=val){
+el.setAttribute(attr,val);
+}
+}
+}
 
 function swapSapphireText(el){
 
