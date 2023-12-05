634f86a89797a3ddf265b72e0e5291565d8a2e83
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-05 01:32:22 +0000

    '

--- api/highlight-js.rb
+++ api/highlight-js.rb
@@ -69,7 +69,7 @@ highlighter();
 
 declare(async ()=>{
 
-let comments=Array.from(document.querySelectorAll('.comment:not([comment="highlight"])''));
+let comments=Array.from(document.querySelectorAll('.comment:not([comment="highlight"])'));
 const comments_length=comments.length;
 for(let i=0;i<comments_length;i++){try{
 
