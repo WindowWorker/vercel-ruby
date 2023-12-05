160bec712a2acde1088847b64eb9887f43dec39a
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-05 02:09:04 +0000

    asdf

--- api/highlight-js.rb
+++ api/highlight-js.rb
@@ -11,7 +11,7 @@ globalThis.highlighter=async function(){
   if(document.querySelector('[class*="language-"]:not(:has(.token))')){
   try{
     if(globalThis.Prism){
-      let hs=Array.from(document.querySelectorAll('[class*="language-"]:not(:has(.token))'));
+      let hs=Array.from(document.querySelectorAll('[class*="language-"]:not(:has(.token,a))'));
       let hs_length=hs.length;
       for(let i=0 ; i<hs_length ; i++){try{
         await Prism.highlightElement?.(hs[i]);
@@ -28,28 +28,28 @@ declare(async ()=>{
 if(!(document.querySelector('link[href="https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css"]'))){
   let prismCss = document.createElement('link');
   prismCss.setAttribute('href','https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism.min.css');
-  prismCss.setAttribute('rel','stylesheet');
+  prismCss.updateAttribute('rel','stylesheet');
   document.firstElementChild.appendChild(prismCss);
 }
 
 await import('https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js');
 await import('https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/components/prism-ruby.min.js');
 
-let prs=Array.from(document.querySelectorAll('pre:not([class])'));
+let prs=Array.from(document.querySelectorAll('pre:not([class],:has(a))'));
 const prs_length=prs.length;
 for(let i=0;i<prs_length;i++){try{
   prs[i].innerHTML='<code class="language-ruby">'+prs[i].innerHTML+'</code>';
-  prs[i].setAttribute('class','language-ruby');
+  prs[i].updateAttribute('class','language-ruby');
 }catch(e){await console.log(e); continue;}}
 
-let notprs=Array.from(document.querySelectorAll(':not(pre)>code'));
+let notprs=Array.from(document.querySelectorAll(':not(pre)>code:not(:has(a))'));
 const notprs_length=notprs.length;
 for(let i=0;i<notprs_length;i++){try{
-  notprs[i].setAttribute('class','language-ruby');
+  notprs[i].updateAttribute('class','language-ruby');
   notprs[i].outerHTML='<pre class="language-ruby">'+notprs[i].outerHTML+'</pre>';
 }catch(e){await console.log(e); continue;}}
 
-let aprs=Array.from(document.querySelectorAll('pre[class*="language-"]:not(:has(code,.token))'));
+let aprs=Array.from(document.querySelectorAll('pre[class*="language-"]:not(:has(code,.token,a))'));
 const aprs_length=aprs.length;
 for(let i=0;i<aprs_length;i++){try{
 if(aprs[i].innerHTML.toString().includes('#highlight#')){
