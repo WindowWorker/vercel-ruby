bfbecba9a563b3f4c47b2e27c2db9b0e65eb7569
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-05 00:48:14 +0000

    asdf

--- api/highlight-js.rb
+++ api/highlight-js.rb
@@ -38,7 +38,7 @@ await import('https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/components/p
 let prs=Array.from(document.querySelectorAll('pre:not([class])'));
 const prs_length=prs.length;
 for(let i=0;i<prs_length;i++){try{
-  prs[i].innerHTML='<code class="language-ruby">'+prs[i].innerHTML+'</code>';
+  prs[i].innerHTML='<code class="language-ruby">#\n'+prs[i].innerHTML+'</code>';
   prs[i].setAttribute('class','language-ruby');
 }catch(e){await console.log(e); continue;}}
 
@@ -46,13 +46,13 @@ let notprs=Array.from(document.querySelectorAll(':not(pre)>code'));
 const notprs_length=notprs.length;
 for(let i=0;i<notprs_length;i++){try{
   notprs[i].setAttribute('class','language-ruby');
-  notprs[i].outerHTML='<pre class="language-ruby">'+notprs[i].outerHTML+'</pre>';
+  notprs[i].outerHTML='<pre class="language-ruby">#\n'+notprs[i].outerHTML+'</pre>';
 }catch(e){await console.log(e); continue;}}
 
 let aprs=Array.from(document.querySelectorAll('pre[class*="language-"]:not(:has(code))'));
 const aprs_length=aprs.length;
 for(let i=0;i<aprs_length;i++){try{
-  aprs[i].innerHTML='<code class="language-ruby">'+aprs[i].innerHTML+'</code>';
+  aprs[i].innerHTML='<code class="language-ruby">#\n'+aprs[i].innerHTML+'</code>';
 }catch(e){await console.log(e); continue;}}
 
 if(document.querySelector('[class*="language-"]')){
