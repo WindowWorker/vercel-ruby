583bbee221c6bcf372c30991c9a974ca9d86132e
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-05 00:34:56 +0000

    asfg

--- api/highlight-js.rb
+++ api/highlight-js.rb
@@ -34,12 +34,21 @@ if(!(document.querySelector('link[href="https://cdnjs.cloudflare.com/ajax/libs/p
 
 await import('https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js');
 await import('https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/components/prism-ruby.min.js');
+
 let prs=Array.from(document.querySelectorAll('pre:not([class])'));
 const prs_length=prs.length;
 for(let i=0;i<prs_length;i++){try{
   prs[i].innerHTML='<code class="language-ruby">'+prs[i].innerHTML+'</code>';
   prs[i].setAttribute('class','language-ruby');
 }catch(e){await console.log(e); continue;}}
+
+let notprs=Array.from(document.querySelectorAll(':not(pre)>code'));
+const notprs_length=notprs.length;
+for(let i=0;i<notprs_length;i++){try{
+  notprs[i].setAttribute('class','language-ruby');
+  notprs[i].outerHTML='<pre class="language-ruby">'+notprs[i].outerHTML+'</pre>';
+}catch(e){await console.log(e); continue;}}
+
 if(document.querySelector('[class*="language-"]')){
 highlighter();
 }
